87230dee5d41df2eb979a50b664dd56002be3982
Aldo Puga <aldo_puga@elude.in>
2019-08-10 17:31:13 -0300

    replace console.log to console.warn when the browser doesn't support service workerers

--- speakClient.js
+++ speakClient.js
@@ -2,7 +2,7 @@ var speakWorker;
 try {
   speakWorker = new Worker('speakWorker.js');
 } catch(e) {
-  console.log('speak.js warning: no worker support');
+  console.warn('speak.js warning: no worker support');
 }
 
 function speak(text, args) {
