19d2b69e926ce09784198a2a285b648c701d5d86
Aldo Puga <aldo_puga@elude.in>
2019-08-13 22:57:59 -0300

    remove profiling

--- speakClient.js
+++ speakClient.js
@@ -31,8 +31,6 @@ try {
 }
 
 function speak(text, args, callback) {
-  var PROFILE = 1;
-
   function parseWav(wav) {
     function readInt(i, bytes) {
       var ret = 0;
@@ -55,24 +53,18 @@ function speak(text, args, callback) {
   }
 
   function handleWav(wav) {
-    var startTime = Date.now();
     var data = parseWav(wav); // validate the data and parse it
-    // TODO: try playAudioDataAPI(data), and fallback if failed
-    if (PROFILE) console.log('speak.js: wav processing took ' + (Date.now()-startTime).toFixed(2) + ' ms');
     callback(data, wav);
   }
 
   if (args && (args.noWorker || !suportServiceWork)) {
     // Do everything right now. speakGenerator.js must have been loaded.
-    var startTime = Date.now();
     var wav = generateSpeech(text, args);
-    if (PROFILE) console.log('speak.js: processing took ' + (Date.now()-startTime).toFixed(2) + ' ms');
     handleWav(wav);
   } else {
     // Call the worker, which will return a wav that we then play
     var startTime = Date.now();
     speakWorker.onmessage = function(event) {
-      if (PROFILE) console.log('speak.js: worker processing took ' + (Date.now()-startTime).toFixed(2) + ' ms');
       handleWav(event.data);
     };
     speakWorker.postMessage({ text: text, args: args });
